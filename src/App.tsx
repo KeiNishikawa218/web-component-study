3f573a8cdd56b74040bb60c3ddba46fab915d68c
k-nishikawa <k-nishikawa@atware.co.jp>
2022-03-12 17:53:57 +0900

    add public dict

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {SimpleGreeting} from './lib/web-components/simple-greeting'
 import {createComponent} from '@lit-labs/react';
